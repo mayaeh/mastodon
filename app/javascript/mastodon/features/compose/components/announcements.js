9a8139de731d7a7853b62e137b418c15c417454b
Maya Minatsuki <mayaeh@marimo-net.org>
2018-05-29 21:49:43 +0900

    Change announcements file path.

--- app/javascript/mastodon/features/compose/components/announcements.js
+++ app/javascript/mastodon/features/compose/components/announcements.js
@@ -76,7 +76,7 @@ export default class Announcements extends React.PureComponent {
     // files in /system/ will be cached by SW
     if (self.caches) {
       return caches.open('mastodon-system')
-        .then(cache => cache.delete(window.origin + '/system/announcements.json'))
+        .then(cache => cache.delete(window.origin + '/announcements.json'))
         .catch(() => {});
     } else {
       return Promise.resolve();
@@ -86,7 +86,7 @@ export default class Announcements extends React.PureComponent {
   refresh = () => {
     this.timer = null;
 
-    axios.get('/system/announcements.json', {
+    axios.get('/announcements.json', {
       headers: {
         'If-Modified-Since': !Announcements.isCacheControlled && Announcements.lastDate || '',
       },
