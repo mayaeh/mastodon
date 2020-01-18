98de59b8750364eea3a28219c48c0401aaab813e
Maya Minatsuki <mayaeh@marimo-net.org>
2020-01-18 13:06:34 +0900

    Improved unlisted visibility

--- app/javascript/mastodon/features/status/components/action_bar.js
+++ app/javascript/mastodon/features/status/components/action_bar.js
@@ -263,6 +263,7 @@ class ActionBar extends React.PureComponent {
     let reblogIcon = 'retweet';
     if (status.get('visibility') === 'direct') reblogIcon = 'envelope';
     else if (status.get('visibility') === 'private') reblogIcon = 'lock';
+    else if (status.get('visibility') === 'unlisted') reblogIcon = 'unlock';
 
     let reblog_disabled = (status.get('visibility') === 'direct' || status.get('visibility') === 'private');
 
