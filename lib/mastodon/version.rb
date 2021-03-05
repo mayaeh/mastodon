75dc9c2ff48f28f5ae45430c022d9262eae713bc
Maya Minatsuki <mayaeh@marimo-net.org>
2021-03-05 13:46:37 +0900

    Update github repository setting.

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -33,7 +33,7 @@ module Mastodon
     end
 
     def repository
-      ENV.fetch('GITHUB_REPOSITORY', 'mosaxiv/mastodon')
+      ENV.fetch('GITHUB_REPOSITORY', 'mayaeh/mastodon')
     end
 
     def source_base_url
