abf4afe4bffe719a49456e155d1acc9b3cb924c8
Maya Minatsuki <mayaeh@marimo-net.org>
2021-12-11 08:43:13 +0900

    Reduce the trend threshold

--- app/models/trends/tags.rb
+++ app/models/trends/tags.rb
@@ -4,8 +4,8 @@ class Trends::Tags < Trends::Base
   PREFIX = 'trending_tags'
 
   self.default_options = {
-    threshold: 15,
-    review_threshold: 10,
+    threshold: 5,
+    review_threshold: 4,
     max_score_cooldown: 2.days.freeze,
     max_score_halflife: 4.hours.freeze,
   }
