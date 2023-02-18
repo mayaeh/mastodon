1e6e451001dee7ac433ca45179cb4da95ea3b8b9
Maya Minatsuki <mayaeh@marimo-net.org>
2023-02-18 17:45:14 +0900

    Changed to be able to change the display position of the publish button on SmartPhone

--- app/models/user.rb
+++ app/models/user.rb
@@ -135,8 +135,7 @@ class User < ApplicationRecord
            :reduce_motion, :system_font_ui, :noindex, :theme, :display_media,
            :expand_spoilers, :default_language, :aggregate_reblogs, :show_application,
            :advanced_layout, :use_blurhash, :use_pending_items, :trends, :crop_images,
-           :disable_swiping, :always_send_emails, :navigation_panel_layout,
-
+           :disable_swiping, :always_send_emails, :navigation_panel_layout, :fab_layout,
            to: :settings, prefix: :setting, allow_nil: false
 
   delegate :can?, to: :role
