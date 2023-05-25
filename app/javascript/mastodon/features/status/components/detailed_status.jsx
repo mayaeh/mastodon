3f1e22be1fc6b607d7978bbf348b34d9b7a2faa3
Maya Minatsuki <mayaeh@marimo-net.org>
2023-05-25 14:09:54 +0900

    fix can't open detailed status page in WebUI.

--- app/javascript/mastodon/features/status/components/detailed_status.jsx
+++ app/javascript/mastodon/features/status/components/detailed_status.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 
-import { FormattedDate, FormattedMessage } from 'react-intl';
+import { injectIntl, FormattedDate, FormattedMessage } from 'react-intl';
 
 import classNames from 'classnames';
 import { Link } from 'react-router-dom';
