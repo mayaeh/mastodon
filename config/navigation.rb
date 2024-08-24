7150fe6d37d5aafe9439003c801a2eb89558689c
Maya Minatsuki <mayaeh@marimo-net.org>
2024-08-24 15:26:04 +0900

    fix no method error

--- config/navigation.rb
+++ config/navigation.rb
@@ -67,7 +67,7 @@ SimpleNavigation::Configuration.run do |navigation|
       s.item :custom_emojis, safe_join([material_symbol('mood'), t('admin.custom_emojis.title')]), admin_custom_emojis_path, highlights_on: %r{/admin/custom_emojis}, if: -> { current_user.can?(:manage_custom_emojis) }
       s.item :webhooks, safe_join([material_symbol('inbox'), t('admin.webhooks.title')]), admin_webhooks_path, highlights_on: %r{/admin/webhooks}, if: -> { current_user.can?(:manage_webhooks) }
       s.item :relays, safe_join([material_symbol('captive_portal'), t('admin.relays.title')]), admin_relays_path, highlights_on: %r{/admin/relays}, if: -> { !limited_federation_mode? && current_user.can?(:manage_federation) }
-      s.item :push_subscription_blocks, safe_join([fa_icon('ban fw'), t('admin.push_subscription_blocks.title')]), admin_push_subscription_blocks_path, highlights_on: %r{/admin/push_subscription_blocks}, if: -> { !limited_federation_mode? && current_user.can?(:manage_federation) }
+      s.item :push_subscription_blocks, safe_join([material_symbol('hide_source'), t('admin.push_subscription_blocks.title')]), admin_push_subscription_blocks_path, highlights_on: %r{/admin/push_subscription_blocks}, if: -> { !limited_federation_mode? && current_user.can?(:manage_federation) }
     end
 
     n.item :sidekiq, safe_join([material_symbol('diamond'), 'Sidekiq']), sidekiq_path, link_html: { target: 'sidekiq' }, if: -> { current_user.can?(:view_devops) }
