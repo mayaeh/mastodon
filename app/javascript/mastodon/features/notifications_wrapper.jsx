1d82066b180cf6cac9b0ee4ca98e2d3c49d59582
Maya Minatsuki <mayaeh@marimo-net.org>
2024-12-08 11:02:08 +0900

    revert

--- app/javascript/mastodon/features/notifications_wrapper.jsx
+++ app/javascript/mastodon/features/notifications_wrapper.jsx
@@ -1,8 +1,13 @@
+import Notifications from 'mastodon/features/notifications';
 import Notifications_v2 from 'mastodon/features/notifications_v2';
+import { selectUseGroupedNotifications } from 'mastodon/selectors/settings';
+import { useAppSelector } from 'mastodon/store';
 
 export const NotificationsWrapper = (props) => {
+  const optedInGroupedNotifications = useAppSelector(selectUseGroupedNotifications);
+
   return (
-    <Notifications_v2 {...props} />
+    optedInGroupedNotifications ? <Notifications_v2 {...props} /> : <Notifications {...props} />
   );
 };
 
